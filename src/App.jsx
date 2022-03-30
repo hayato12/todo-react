450447d828eb669ba32b95d10e2a6ab55b86f34f
hayato12 <50575834+hayato12@users.noreply.github.com>
2022-03-30 17:33:58 +0900

    Reactのstyleとイベントの扱い

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const App = () => {
+  const onClickButton = () => alert();
+  const contentStyle = {
+    color: "blue",
+    fontSize: "18px"
+  };
+  const contentPinkStyle = {
+    color: "pink",
+    fontSize: "18px"
+  };
+  return (
+    <>
+      <h1 style={{ color: "red" }}>こんにちは</h1>
+      <p style={contentStyle}>お元気ですか？</p>
+      <p style={contentPinkStyle}>お元気です？</p>
+      <button onClick={onClickButton}>ボタン</button>
+    </>
+  );
+};
+
+export default App;
