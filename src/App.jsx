ff70e278440731b7f9d7a203cf0b2fc64c10763f
hayato12 <50575834+hayato12@users.noreply.github.com>
2022-03-30 18:25:31 +0900

    さいレンダリングとその副作用(useEffect)

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,34 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
+
   const onClickCountUp = () => {
     setNum(num + 1);
   };
-  const [num, setNum] = useState(0);
+  const onCLickSwitchShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+  useEffect(() => {
+    if (num % 3 === 0 && num !== 0) {
+      faceShowFlag || setFaceShowFlag(true);
+    } else {
+      faceShowFlag && setFaceShowFlag(false);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [num]);
+
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは</h1>
       <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
       <ColorfulMessage color="pink">お元気です？</ColorfulMessage>
       <button onClick={onClickCountUp}>カウントアップ</button>
+      <button onClick={onCLickSwitchShowFlag}>on/off</button>
       <p>{num}</p>
+      {faceShowFlag && <p>^^^^</p>}
     </>
   );
 };
